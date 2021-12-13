28bc557f7e521ed722fbe9cb5081f3450c274bdd
cerqueirav <victorcerx@gmail.com>
2021-12-13 03:11:14 -0300

    dart study

---
+++ src/Boate.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+main() {
+  var idade = stdin.readLineSync();
+  entrarNaBoate(idade);
+}
+
+void entrarNaBoate(var text) {
+  var idade = converterParaInteiro(text);
+  if (idadeEhValida(idade)) {
+    if (idadeEhPermitida(idade))
+      print("[OK] - Cliente autorizado! ");
+    else
+      print("[X] - Cliente nao autorizado, pois, eh menor de idade!");
+  } else {
+    print("[X] - Idade invalida!");
+  }
+}
+
+bool idadeEhPermitida(int idade) {
+  if (idade >= 18) return true;
+
+  return false;
+}
+
+bool idadeEhValida(int idade) {
+  if ((idade < 0) | (idade > 110)) return false;
+
+  return true;
+}
+
+int converterParaInteiro(var idadeString) {
+  return (int.parse(idadeString));
+}
